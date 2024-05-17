24a9fd569df01208d32f628bb8e2d54383296f04
ah7255703 <ah7255703@gmail.com>
2024-05-17 03:28:32 +0300

    chore: Add napi app with Dockerfile and configuration files

---
+++ apps/napi/src/index.ts
@@ -0,0 +1,14 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+const port = 3000
+const server = serve({
+  fetch: app.fetch,
+  port
+})
